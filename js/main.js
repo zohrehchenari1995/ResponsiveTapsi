79401baa5cf85e6f23ae7387e419328dc2920970
zohrehchenari1995 <zohrehchenari2874@gmail.com>
2024-09-09 09:02:24 +0430

    added header component

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+const toggler = document.querySelector(".nav__toggler");
+const nav = document.querySelector(".nav");
+toggler.addEventListener("click",(e)=>{
+  nav.classList.toggle("nav__expanded");
+});
